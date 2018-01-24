853d4bed18e414b7f3d0e4263a4bec2a3bb3e53e
ajbinky <ajbinky@gmail.com>
2018-01-24 13:04:43 -0600

    working

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace dllInjector
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInject = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.cb = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInject
+            // 
+            this.btnInject.Location = new System.Drawing.Point(12, 150);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(151, 54);
+            this.btnInject.TabIndex = 0;
+            this.btnInject.Text = "Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            this.btnInject.Click += new System.EventHandler(this.btnInject_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "none";
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(12, 95);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(151, 49);
+            this.btnFile.TabIndex = 1;
+            this.btnFile.Text = "Choose dll file";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // cb
+            // 
+            this.cb.FormattingEnabled = true;
+            this.cb.Location = new System.Drawing.Point(12, 68);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(151, 21);
+            this.cb.TabIndex = 2;
+            this.cb.Text = "Process to inject";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(12, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(151, 50);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh processes";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 207);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(20, 13);
+            this.lblPath.TabIndex = 4;
+            this.lblPath.Text = "file";
+            this.lblPath.Visible = false;
+            // 
+            // formMain
+            // 
+            this.AcceptButton = this.btnInject;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(175, 244);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cb);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.btnInject);
+            this.Name = "formMain";
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInject;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.ComboBox cb;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label lblPath;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace dllInjector
+{
+    public partial class formMain : Form
+    {
+
+        string path;
+        string[] injectable = { "iw3mp", "iw4mp", "iw5mp", "cabalmain" };
+
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32")]
+        public static extern IntPtr CreateRemoteThread(
+          IntPtr hProcess,
+          IntPtr lpThreadAttributes,
+          uint dwStackSize,
+          UIntPtr lpStartAddress,
+          IntPtr lpParameter,
+          uint dwCreationFlags,
+          out IntPtr lpThreadId
+        );
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(
+            UInt32 dwDesiredAccess,
+            Int32 bInheritHandle,
+            Int32 dwProcessId
+            );
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(
+        IntPtr hObject
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool VirtualFreeEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            UIntPtr dwSize,
+            uint dwFreeType
+            );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true)]
+        public static extern UIntPtr GetProcAddress(
+            IntPtr hModule,
+            string procName
+            );
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            uint dwSize,
+            uint flAllocationType,
+            uint flProtect
+            );
+
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            string lpBuffer,
+            UIntPtr nSize,
+            out IntPtr lpNumberOfBytesWritten
+        );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(
+            string lpModuleName
+            );
+
+        [DllImport("kernel32", SetLastError = true, ExactSpelling = true)]
+        internal static extern Int32 WaitForSingleObject(
+            IntPtr handle,
+            Int32 milliseconds
+            );
+
+        public Int32 GetProcessId(String proc)
+        {
+            Process[] ProcList;
+            ProcList = Process.GetProcessesByName(proc);
+            return ProcList[0].Id;
+        }
+
+        public void InjectDLL(IntPtr hProcess, String strDLLName)
+        {
+            IntPtr bytesout;
+            
+            Int32 LenWrite = strDLLName.Length + 1;
+            IntPtr AllocMem = (IntPtr)VirtualAllocEx(hProcess, (IntPtr)null, (uint)LenWrite, 0x1000, 0x40);
+            
+            WriteProcessMemory(hProcess, AllocMem, strDLLName, (UIntPtr)LenWrite, out bytesout);
+            UIntPtr Injector = (UIntPtr)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+            if (Injector == null)
+            {
+                MessageBox.Show(" Injector Error! \n ");
+                return;
+            }
+            
+            IntPtr hThread = (IntPtr)CreateRemoteThread(hProcess, (IntPtr)null, 0, Injector, AllocMem, 0, out bytesout);
+            if (hThread == null)
+            {
+                MessageBox.Show(" hThread [ 1 ] Error! \n ");
+                return;
+            }
+            int Result = WaitForSingleObject(hThread, 10 * 1000);
+            if (Result == 0x00000080L || Result == 0x00000102L || Result == 0xFFFFFFFF)
+            {
+                MessageBox.Show(" hThread [ 2 ] Error! \n ");
+                if (hThread != null)
+                {
+                    CloseHandle(hThread);
+                }
+                return;
+            }
+            Thread.Sleep(1000);
+            VirtualFreeEx(hProcess, AllocMem, (UIntPtr)0, 0x8000);
+            if (hThread != null)
+            {
+                CloseHandle(hThread);
+            }
+            return;
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                path = ofd.FileName;
+                lblPath.Text = path;
+                lblPath.Visible = true;
+            }
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            loadProcesses();
+        }
+
+        private void loadProcesses()
+        {
+            Process[] processlist = Process.GetProcesses();
+            var list = new List<Process>();
+            foreach (Process p in processlist)
+            {
+                if (injectable.Contains(p.ProcessName))
+                {
+                    list.Add(p);
+                }
+            }
+            cb.DataSource = list;
+            cb.DisplayMember = "ProcessName";
+            cb.ValueMember = "ProcessName";
+            cb.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadProcesses();
+        }
+
+        private void btnInject_Click(object sender, EventArgs e)
+        {
+            if (path == null)
+            {
+                MessageBox.Show("Please choose dll file");
+            } else if (cb.Text == "" || cb.Text == null)
+            {
+                MessageBox.Show("Please choose a valid process");
+            } else
+            {
+                Int32 ProcID = GetProcessId(cb.Text);
+                if (ProcID >= 0)
+                {
+                    IntPtr hProcess = (IntPtr)OpenProcess(0x1F0FFF, 1, ProcID);
+                    if (hProcess == null)
+                    {
+                        MessageBox.Show("OpenProcess() Failed!");
+                        return;
+                    }
+                    else
+                        InjectDLL(hProcess, path);
+                }
+            }
+        }
+    }
+}
